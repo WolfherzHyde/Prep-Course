4ce382b99e341bd69deac6d37d317b49f92848c3
WORLFHERZHYDE <bruperseg@gmail.com>
2022-09-25 19:45:32 -0500

    prueba 1

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,19 +4,19 @@
 const nuevaString = 'Hyde';
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = 27;
+const nuevoNum = 27 ;
 
 // Crea una variable booleana:
-const nuevoBool = 'false';
+const nuevoBool = false ;
 
 // Resuelve el siguiente problema matemático:
-const nuevaResta = 10 - 5 === 5;
+const nuevaResta = 10 - 5 == 5;
 
 // Resuelve el siguiente problema matemático:
 const nuevaMultiplicacion = 10 * 4 === 40 ;
 
 // Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === 1;
+const nuevoModulo = 21 % 5 == 2.7;
 
 
 // En los próximos 22 problemas, deberás completar la función.
