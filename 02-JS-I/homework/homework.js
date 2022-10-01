2ed5c22c9aeb59f8257c8288dc9a38c8346d01b9
unknown <bruperseg@gmail.com>
2022-10-01 10:16:49 -0500

    resolvedospuntocero

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -136,20 +136,14 @@ function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
-  var num = 4
-  if () {
-    
-  } else {
-    
-  }
+
 }
 
 function elevarAlCuadrado(num) {
   // Devuelve el valor de "num" elevado al cuadrado
   // ojo: No es raiz cuadrada!
   // Tu código:
-  var num = 2**
-  return num
+
 
 }
 
