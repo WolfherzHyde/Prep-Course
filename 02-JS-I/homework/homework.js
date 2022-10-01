292f214dff081aa477df21c02cf6b257a0e01eaf
unknown <bruperseg@gmail.com>
2022-10-01 11:44:26 -0500

    revision34

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -185,7 +185,7 @@ function redondearHaciaArriba(num) {
   return NumeroMasCercanoHaciaArriba
 }
 
-function numeroRandom(0, 1) {
+function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
   var NumeroRandom = math.random(0, 1)
