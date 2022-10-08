b994a4a781a8df2117cd3e4aca594ecd5698f0b4
wolfherzhyde <bruperseg@gmail.com>
2022-10-08 05:05:34 -0500

    no entiendo por que esta bien esprimo

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -186,13 +186,31 @@ else
 }
 
 
-function esPrimo(numero) {
+
   // Devuelve "true" si "numero" es primo
   // De lo contrario devuelve "falso"
+  function esPrimo(numero) {
+    if (numero < 2) {
+      return false;
+    } 
+
+    for (i = 2; i < numero ; i++) {
+      if (numero % i === 0) {
+        return false;
+      } 
+}     
+    return true;
+}
+
+
+
+        
+
+
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
-}
+
 
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
