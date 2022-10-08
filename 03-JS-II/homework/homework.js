344f60077ad3de6a85b9a0db7bec20cdf99ade45
wolfherzhyde <bruperseg@gmail.com>
2022-10-08 01:51:01 -0500

    vamos pien

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -158,12 +158,33 @@ function fizzBuzz(numero) {
 
 function operadoresLogicos(num1, num2, num3) {
   //La función recibe tres números distintos. 
-  //Si num1 es mayor a num2 y a num3 y además es positivo, retornar ---> "Número 1 es mayor y positivo"
-  //Si alguno de los tres números es negativo, retornar ---> "Hay negativos"
-  //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
-  //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
+
+
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+
+   //Si alguno de los tres números es negativo, retornar ---> "Hay negativos"
+  if(0 > num1 || 0 > num2 || 0 > num3){
+    return("Hay negativos")
+  }
+  //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
+  else if(num1 == 0 || num2 == 0  || num3  == 0){
+    return("Error")}
+  //Si num1 es mayor a num2 y a num3 y además es positivo, retornar ---> "Número 1 es mayor y positivo"
+  else if(num1 > 0 && num1 > num2 && num1 > num3){
+  return("Número 1 es mayor y positivo");
 }
+
+
+
+  //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
+  if(num3 > num1 && num3 > num2 ){
+    return(num3 +1);
+  }
+ 
+else
+  return (false)
+}
+
 
 function esPrimo(numero) {
   // Devuelve "true" si "numero" es primo
